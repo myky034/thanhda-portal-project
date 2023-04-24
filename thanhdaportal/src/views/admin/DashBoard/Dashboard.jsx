0fc94cf98faeb06d23a2070adc65fee98a453706
myky034 <vumyky.034@gmail.com>
2023-04-24 09:42:22 +0700

    Update Sidebar and Dashboard

--- thanhdaportal/src/views/admin/DashBoard/Dashboard.jsx
+++ thanhdaportal/src/views/admin/DashBoard/Dashboard.jsx
@@ -1,24 +1,22 @@
 import React from "react";
 import "./DashBoard.scss";
 import Sidebar from "../../../components/SideBar/Sidebar";
-import { Box, Typography } from "@mui/material";
-import { styled, useTheme } from "@mui/material/styles";
+import { Box } from "@mui/material";
 import WelcomeBox from "./../../../components/WelcomeBox/WelcomeBox";
-import Calendar from "../../../components/Calendar/CalendarBox";
 import CalendarBox from "../../../components/Calendar/CalendarBox";
 import News from "./News/News";
 import EventsBox from "./EventsBox/EventsBox";
 import ActivityLog from "./ActivityLog/ActivityLog";
 
 const Dashboard = () => {
-  const DrawerHeader = styled("div")(({ theme }) => ({
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "flex-end",
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-  }));
+  // const DrawerHeader = styled("div")(({ theme }) => ({
+  //   display: "flex",
+  //   alignItems: "center",
+  //   justifyContent: "flex-end",
+  //   padding: theme.spacing(0, 1),
+  //   // necessary for content to be below app bar
+  //   ...theme.mixins.toolbar,
+  // }));
 
   return (
     <Box className="dashboard-box" component="main" sx={{ flexGrow: 1, p: 3 }}>
